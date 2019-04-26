249dbcce85ddc61efdcf927519b83f3524c0aac4
CesarRivasP <cesarjose58@gmail.com>
2019-04-26 14:59:43 -0400

    Implemented all messages propagation

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -20,19 +20,29 @@ io.on('connection', (client) => {
   // Hay que implementar para poder escuchar el mensaje
   // -- Escuchar al cliente
   //        (nombre del evento, recibir evento, funcion a llamar cuando todo salio bien o se realiza la accion)
-  client.on('enviarMensaje', (message, callback) => {
-    console.log(message);
+  client.on('enviarMensaje', (data, callback) => {
+    console.log(data);
 
-    //El callback se puede llamar x cantidad de veces
-    if(message.user ) { //Si viene el usuario
-      callback({
-        response: 'Todo salio bien!'
-      });
-    }
-    else {
-      callback({
-        response: 'Todo salio mal!!'
-      })
-    }
+    // EMitir a todos los usuarios
+    // cliente.emit('enviarMensaje', {
+    //   user: data.user,
+    //   message: data.message
+    // })
+    // O
+    client.broadcast.emit('enviarMensaje', data)
+    // broadcast indica que la propagacion del mensaje es para todo los que
+    // estan conectados al canal
+
+  //El callback se puede llamar x cantidad de veces
+  //   if(message.user ) { //Si viene el usuario
+  //     callback({
+  //       response: 'Todo salio bien!'
+  //     });
+  //   }
+  //   else {
+  //     callback({
+  //       response: 'Todo salio mal!!'
+  //     })
+  //   }
   })
 })
