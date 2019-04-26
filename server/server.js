a7d8e12427d7f74fe9cf5bf2502def8528d0d4f1
CesarRivasP <cesarjose58@gmail.com>
2019-04-26 13:13:36 -0400

    Refactoring io object

--- server/server.js
+++ server/server.js
@@ -17,47 +17,10 @@ app.use(express.static(publicPath));
 // Hay que inicializar el socketIO
 // IO hace referencia a inputs y outputs
 // io va a mantener una conexion directa con el servidor, esta es la comunicacion del backend
-let io = socketIO(server);
+// let io = socketIO(server); BEFORE
+module.exports.io = socketIO(server);  // AFTER
 
-// client contiene toda la informacion de la computadora o conexion que se establecio
-io.on('connection', (client) => {
-  console.log('Usuario conectado');
-
-  // Emitir desde el servidor, escuchar en el cliente
-  client.emit('enviarMensaje', {
-    // user: 'Administrador',
-    message: 'Bienvenido a esta aplicacion'
-  })
-
-
-  client.on('disconnect', () => {
-    console.log('Usuario desconectado');
-  })
-
-
-  // Para recibir la informacion en el servidor
-  // Hay que implementar para poder escuchar el mensaje
-  // -- Escuchar al cliente
-  //        (nombre del evento, recibir evento, funcion a llamar cuando todo salio bien o se realiza la accion)
-  client.on('enviarMensaje', (message, callback) => {
-    console.log(message);
-
-    //El callback se puede llamar x cantidad de veces
-    if(message.user ) { //Si viene el usuario
-      callback({
-        response: 'Todo salio bien!'
-      });
-    }
-    else {
-      callback({
-        response: 'Todo salio mal!!'
-      })
-    }
-  })
-})
-
-
-
+require('./sockets/socket');
 
 // app.listen(port, (error) => {
 server.listen(port, (error) => {  //para usar con sockets
